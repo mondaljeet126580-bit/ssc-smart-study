db51b705fe0cd9842a27ad03cbdf7ce3d8406e5a
mondaljeet126580-bit <mondaljeet126580@gmail.com>
2026-07-28 10:45:14 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,14 +1,49 @@
-console.log("SSC Smart Study Started");
+document.addEventListener("DOMContentLoaded", async () => {
 
-async function loadSubjects() {
-    try {
-        const response = await fetch("data/subjects.json");
-        const subjects = await response.json();
+    const searchInput = document.querySelector(".search-box input");
+    const menuGrid = document.querySelector(".menu-grid");
 
-        console.log(subjects);
-    } catch (error) {
-        console.error(error);
+    let subjects = [];
+
+    async function loadSubjects() {
+        try {
+            const response = await fetch("data/subjects.json");
+            subjects = await response.json();
+            displaySubjects(subjects);
+        } catch (error) {
+            console.log("Error loading subjects:", error);
+        }
     }
-}
 
-loadSubjects();
+    function displaySubjects(list) {
+
+        menuGrid.innerHTML = "";
+
+        list.forEach(subject => {
+
+            menuGrid.innerHTML += `
+                <div class="card">
+                    <div class="icon">${subject.icon}</div>
+                    <h3>${subject.name}</h3>
+                </div>
+            `;
+
+        });
+
+    }
+
+    searchInput.addEventListener("input", function () {
+
+        const keyword = this.value.toLowerCase();
+
+        const filtered = subjects.filter(subject =>
+            subject.name.toLowerCase().includes(keyword)
+        );
+
+        displaySubjects(filtered);
+
+    });
+
+    loadSubjects();
+
+});
