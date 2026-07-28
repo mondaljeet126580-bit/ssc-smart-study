7889495d9a6b5bab1858a02873ec5ae77b180937
mondaljeet126580-bit <mondaljeet126580@gmail.com>
2026-07-28 08:47:49 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,10 @@
 
+const app = document.getElementById("app");
+
+async function loadPage(page) {
+    const response = await fetch(page);
+    const html = await response.text();
+    app.innerHTML = html;
+}
+
+loadPage("pages/home.html");
